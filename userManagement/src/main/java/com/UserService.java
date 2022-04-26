612561a65928a49d31b053248f63aad7a32cfd74
Dayan1999 <dayanjayasinghe93@gmail.com>
2022-04-26 22:21:40 +0530

    Final Update

--- userManagement/src/main/java/com/UserService.java
+++ userManagement/src/main/java/com/UserService.java
@@ -1,66 +1,82 @@
 package com;
 
-import model.user;
+import model.User;
 
 //For REST Service
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 //For JSON
-import com.google.gson.*;
-//For XML
+import com.google.gson.*; 
 
+//For XML
 import org.jsoup.*;
 import org.jsoup.parser.*;
 import org.jsoup.nodes.Document;
 
+//View
 @Path("/Users")
 public class UserService
 {
-         user userObj = new user();
-         @GET
-         @Path("/")
-         @Produces(MediaType.TEXT_HTML)
-         public String readItems()
-         {
-        	 return userObj.readItems();
-         }
-         
-         
+	 User userObj = new User();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readUsers()
+	 {
+			return userObj.readUsers();
+	 }
+	
+	//Insert
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertUsers(
+		@FormParam("name") String name,
+		@FormParam("phoneNum") String phoneNum,
+		@FormParam("email") String email,
+		@FormParam("userName") String userName,
+		@FormParam("password") String password)
+	{
+		String output = userObj.insertUsers(name, phoneNum, email, userName, password);
+		return output;
+	}
+	
+	//Update
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateUsers(String userData)
+	{
+		//Convert the input string to a JSON object
+		JsonObject userObject = new JsonParser().parse(userData).getAsJsonObject();
+		//Read the values from the JSON object
+		String userID = userObject.get("userID").getAsString();
+		String name = userObject.get("name").getAsString();
+		String phoneNum = userObject.get("phoneNum").getAsString();
+		String email = userObject.get("email").getAsString();
+		String userName = userObject.get("userName").getAsString();
+		String password = userObject.get("password").getAsString();
+		String output = userObj.updateUsers(userID, name, phoneNum, email, userName, password);
+		return output;
+	}
+	
+	//Delete
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUsers(String userData)
+	{
+		//Convert the input string to an XML document
+		Document doc = Jsoup.parse(userData, "", Parser.xmlParser());
 
-
-@POST
-@Path("/")
-@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-@Produces(MediaType.TEXT_PLAIN)
-public String insertUser(@FormParam("name") String name,
-                    @FormParam("phoneNum") String phoneNum,
-                    @FormParam("email") String email,
-                    @FormParam("userName") String userName,
-                    @FormParam("password") String password)
-                    
-{
-String output = userObj.insertUser(name, phoneNum, email, userName,password);
-return output;
-}
-
-@PUT
-@Path("/")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.TEXT_PLAIN)
-public String updateUser(String userData)
-{
-                  //Convert the input string to a JSON object
-                  JsonObject userObject = new JsonParser().parse(userData).getAsJsonObject();
-                  //Read the values from the JSON object
-                  String userID = userObject.get("userID").getAsString();
-                  String name = userObject.get("name").getAsString();
-                  String phoneNum = userObject.get("phoneNum").getAsString();
-                  String email = userObject.get("email").getAsString();
-                  String userName= userObject.get("userName").getAsString();
-                  String password= userObject.get("password").getAsString();
-                  String output = userObj.updateUser(userID, name, phoneNum, email, userName,password);
-return output;
-}
-
-}
+		//Read the value from the element <userID>
+		String userID = doc.select("userID").text();
+		String output = userObj.deleteUsers(userID);
+		return output;
+	}
+	
+} 
--- userManagement/src/main/java/model/user.java
+++ userManagement/src/main/java/model/user.java
@@ -1,155 +1,193 @@
 package model;
-import java.sql.* ;
 
-public class user
-{
+import java.sql.*;
 
-public Connection connect()
-{
-         Connection con = null;
-          try
-          {
-             Class.forName("com.mysql.jdbc.Driver");
-             con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/usermanagement",
-                                              "root", "");
-//For testing
-          System.out.print("Successfully connected");
-          }
-          catch(Exception e)
-          {
-             e.printStackTrace();
-          }
-return con;
-}
-
-//Insert
-public String insertUser(String Name, String phone, String Email, String UserN,String Password)
-{
-String output = "";
-try
-{
-    Connection con = connect();
-    if (con == null)
-    {
-      return "Error while connecting to the database";
-    }
-    
-    // create a prepared statement
-    String query = " insert into users (`userID`,`name`,`phoneNum`,`email`,`userName`,`password`)"
-   		  + " values ( ?, ?, ?, ?, ?,?)";
-    PreparedStatement preparedStmt = con.prepareStatement(query);
-    
-    // binding values
-    preparedStmt.setInt(1, 0);
-    preparedStmt.setString(2, Name);
-    preparedStmt.setInt(3, Integer.parseInt(phone));
-    preparedStmt.setString(4, Email);
-    preparedStmt.setString(5, UserN);
-    preparedStmt.setString(6, Password);
-   
-    //execute the statement
-    preparedStmt.execute();
-    con.close();
-    
-    output = "Inserted successfully";
- }
- catch (Exception e)
- {
-    output = "Error while inserting";
-    System.err.println(e.getMessage());
- }
-    return output;
-    }
-
-//read
-public String readItems()
-{
-        String output = "";
-        try
-        {
-            Connection con = connect();
-            if (con == null)
-            {
-                 return "Error while connecting to the database for reading.";
-            }
-        // Prepare the html table to be displayed
-        output = "<table border='1'><tr><th>Name</th>"
-               +"<th>Phone Number</th><th>Email</th>"
-               + "<th>User Name</th>"
-               + "<th>Password</th>"
-               + "<th>Update</th><th>Remove</th></tr>";
-        String query = "select * from users";
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-       // iterate through the rows in the result set
-       while (rs.next())
-      {
-               String userID = Integer.toString(rs.getInt("userID"));
-               String name = rs.getString("name");
-               String phoneNum = Integer.toString(rs.getInt("phoneNum"));
-               String email = rs.getString("email");
-               String userName = rs.getString("userName");
-               String password = rs.getString("password");
-               
-       // Add a row into the html table
-       output += "<tr><td>" + name + "</td>";
-       output += "<td>" + phoneNum + "</td>";
-       output += "<td>" + email + "</td>";
-       output += "<td>" + userName + "</td>";
-       output += "<td>" + password + "</td>";
-      
-       //buttons
-       output += "<td><input name='btnUpdate' "
-              + " type='button' value='Update'></td>"
-              + "<td><form method='post' action='users.jsp'>"
-              + "<input name='btnRemove' "
-              + " type='submit' value='Remove'>"
-              + "<input name='userID' type='hidden' "
-              + " value='" + userID + "'>" + "</form></td></tr>";
-        }
-        con.close();
-        //Complete the html table
-        output += "</table>";
-       }
-       catch (Exception e)
-        {
-           output = "Error while reading the users.";
-           System.err.println(e.getMessage());
-        }
-        return output;
-}
-//Update
-public String updateUser(String ID, String name, String phone, String Email, String UserN,String Password)
+public class User
 { 
-	 String output = ""; 
-	 try
-	 { 
-	 Connection con = connect(); 
-	 if (con == null) 
-	 {return "Error while connecting to the database for updating."; } 
-	 // create a prepared statement
+	//A common method to connect to the DB
+	private Connection connect()
+	 {
+		 Connection con = null;
+		 
+		 try
+		 {
+		 Class.forName("com.mysql.jdbc.Driver");
+		
+		 //Provide the correct details: DBServer/DBName, username, password
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/userdb", "root", "");
+		 }
+		 catch (Exception e)
+		 {e.printStackTrace();}
+		 
+		 return con;
+	 }
 	
-	 String query = "UPDATE users SET name=?,phoneNum=?,email=?,userName=?,password=? WHERE userID=?"; 
-	 		        
-			         
-	 PreparedStatement preparedStmt = con.prepareStatement(query); 
-	 // binding values
-	 preparedStmt.setString(1, name); 
-	 preparedStmt.setInt(2,Integer.parseInt (phone)); 
-	 preparedStmt.setString(3,Email); 
-	 preparedStmt.setString(4, UserN); 
-	 preparedStmt.setString(5, Password); 
-	 preparedStmt.setInt(6,Integer.parseInt (ID));
-	 // execute the statement
-	 preparedStmt.execute(); 
-	 con.close(); 
-	 output = "Updated successfully"; 
-	 } 
-	 catch (Exception e) 
-	 { 
-	 output = "Error while updating the item."; 
-	 System.err.println(e.getMessage()); 
-	 } 
-	 return output; 
-	 } 
-}
+	//Insert
+	public String insertUsers(String Name_u, String phone_u, String Email_u, String UserN_u, String Password_u)
+	 {
+		 String output = "";
+		 
+		 try
+		 {
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for inserting."; }
+			 
+			 // create a prepared statement
+			 String query = " insert into users(`userID`,`name`,`phoneNum`,`email`,`userName`,`password`)" + " values (?, ?, ?, ?, ?, ?)";
+			 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, Name_u);
+			 preparedStmt.setString(3, phone_u);
+			 preparedStmt.setString(4, Email_u);
+			 preparedStmt.setString(5, UserN_u);
+			 preparedStmt.setString(6, Password_u);
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 
+			 output = "Inserted successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while inserting the users.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+	}
+	
+	public String readUsers()
+	{
+		 String output = "";
+		 
+		 try
+		 {
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for reading."; }
+			 
+			 // Prepare the html table to be displayed
+			 output = "<table border='1'><tr><th>User ID</th><th>Name</th><th>Phone number</th>" +
+					 "<th>Email</th>" +
+					 "<th>User name</th>" +
+					 "<th>Password</th></tr>";
+			
+			 String query = "select * from users";
+			 Statement stmt = con.createStatement();
+			 ResultSet rs = stmt.executeQuery(query);
+			 
+			 // iterate through the rows in the result set
+			 while (rs.next())
+			 {
+				 String userID = Integer.toString(rs.getInt("userID"));
+				 String name = rs.getString("name");
+				 String phoneNum = rs.getString("phoneNum");
+				 String email = rs.getString("email");
+				 String userName = rs.getString("userName");
+				 String password = rs.getString("password");
+				 
+				 // Add into the html table
+				 output += "<tr><td>" + userID + "</td>";
+				 output += "<td>" + name + "</td>";
+				 output += "<td>" + phoneNum + "</td>";
+				 output += "<td>" + email + "</td>";
+				 output += "<td>" + userName + "</td>";
+				 output += "<td>" + password + "</td></tr>";
+				 
+				 // buttons
+				 //output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>" + "<td><form method='post' action='electricityrequests.jsp'>" + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>" + "<input name='userID' type='hidden' value='" + userID + "'>" + "</form></td></tr>";
+			 }
+			 con.close();
+			 // Complete the html table
+			 output += "</table>";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while reading the items.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+	}
+	
+	//update
+	public String updateUsers(String ID, String Name_u, String phone_u, String Email_u, String UserN_u, String Password_u)
+	{
+		 String output = "";
+		 
+		 try
+		 {
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for updating."; }
+			 
+			 // create a prepared statement
+			 String query = "UPDATE users SET name=?,phoneNum=?,email=?,userName=?,password=? WHERE userID=?";
+			 
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setString(1, Name_u);
+			 preparedStmt.setString(2, phone_u);
+			 preparedStmt.setString(3, Email_u);
+			 preparedStmt.setString(4, UserN_u);
+			 preparedStmt.setString(5, Password_u);
+			 preparedStmt.setInt(6, Integer.parseInt(ID));
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 
+			 output = "Updated successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while updating the user.";
+			 System.err.println(e.getMessage());
+		 }
+		 
+		 return output;
+		 
+	}
+	
+	// delete
+	public String deleteUsers(String userID )
+	{
+		 String output = "";
+		 
+		 try
+		 {
+		 
+			 Connection con = connect();
+			 
+			 if (con == null)
+			 {return "Error while connecting to the database for deleting."; }
+			 
+			 // create a prepared statement
+			 String query = "delete from users where userID=?";
+			 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 
+			 // binding values
+			 preparedStmt.setInt(1, Integer.parseInt(userID ));
+			 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 
+			 output = "Deleted successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while deleting the user.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output;
+		 }
+	}
